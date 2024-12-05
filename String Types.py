b95bd9b3788d4c07acbdda73b10e05d033dcdebe
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 13:56:03 +0000

    Add files via upload

---
+++ String Types.py
@@ -0,0 +1,7 @@
+#String Types by Dc117 Corp.
+
+print('String Types V1.00')
+word = input('Please enter a word> ')
+print('You entered:', word)
+word = word.upper()
+print('In capital letters, your word is:', word)
